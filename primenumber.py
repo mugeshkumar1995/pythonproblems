70dfdc094d5c9fbdaf076dc19fd508760261f5b1
austinmartin <mugeshkumaar@gmail.com>
2018-10-04 17:08:52 +0530

    prime factor

--- primenumber.py
+++ primenumber.py
@@ -1,20 +1,18 @@
-def primefactor(number):
-	def primenumber(number):
-		for i in range(2,number):
-			if number%i==0:
-				pass
-				break
-		else:
-			return number
-	lis=[]
+def primenumber(number):
 	for i in range(2,number):
-		lis.append(primenumber(i))
-	lis1=set(lis)
-	lis1.remove(None)
-	list2=[]
-	for i in lis1:
-	 	if number%i==0:
-	 		list2.append(i)
-	print(list2[-1]) 		
+		if number%i==0:
+			pass
+			break
+
+	else:
+		return number
+def primefactor(number):
+	for i in range(2,number):
+		a=primenumber(i)
+		if a==None:
+			pass
+		else:
+			if number%a==0:
+				print(a)	
 
 primefactor(600851475143)				
